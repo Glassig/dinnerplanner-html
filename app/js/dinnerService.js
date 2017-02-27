8bbf36d5dd76d7b1315a5b51305f80f5a0a31275
Filip <filipjan@kth.se>
2017-02-27 10:55:37 +0100

    dinnerService api

--- app/js/dinnerService.js
+++ app/js/dinnerService.js
@@ -1,11 +1,29 @@
-// Here we create an Angular service that we will use for our 
+// Here we create an Angular service that we will use for our
 // model. In your controllers (or other services) you can include the
 // dependency on any service you need. Angular will insure that the
 // service is created first time it is needed and then just reuse it
 // the next time.
 dinnerPlannerApp.factory('Dinner',function ($resource) {
-  
+
   var numberOfGuest = 2;
+
+  this.DishSearch = $resource('https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search',{},{
+    get: {
+      headers: {
+        'X-Mashape-Key': 'Qu9grxVNWpmshA4Kl9pTwyiJxVGUp1lKzrZjsnghQMkFkfA4LB'
+      }
+    }
+  });
+  this.Dish = $resource('https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/:id/information',{},{
+    get: {
+      headers: {
+         'X-Mashape-Key': 'Qu9grxVNWpmshA4Kl9pTwyiJxVGUp1lKzrZjsnghQMkFkfA4LB'
+      }
+    }
+  });
+
+  
+
 
 
   this.setNumberOfGuests = function(num) {
@@ -19,7 +37,7 @@ dinnerPlannerApp.factory('Dinner',function ($resource) {
 
   // TODO in Lab 5: Add your model code from previous labs
   // feel free to remove above example code
-  // you will need to modify the model (getDish and getAllDishes) 
+  // you will need to modify the model (getDish and getAllDishes)
   // a bit to take the advantage of Angular resource service
   // check lab 5 instructions for details
 
