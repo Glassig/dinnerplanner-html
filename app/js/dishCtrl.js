fed981f29869da1f4ff406c0b164af45aceb3bb9
Filipjansson95 <filipjansson0@gmail.com>
2017-02-27 14:13:08 +0100

    Dishview basic functions implemented

--- app/js/dishCtrl.js
+++ app/js/dishCtrl.js
@@ -1,9 +1,21 @@
 // Dinner controller that we use whenever we want to display detailed
 // information for one dish
 dinnerPlannerApp.controller('DishCtrl', function ($scope,$routeParams,Dinner) {
-  
+  $scope.dishID = $routeParams.dishId;
+  $scope.dish ="";
+  $scope.getInfo = function(){
+  Dinner.Dish.get({id: $scope.dishID},function(data){
+      $scope.status = "Searching..."
+      console.log(data);
+      $scope.dish = data;
+      $scope.ttlCost = Dinner.oneDishCost($scope.dish);
+      $scope.status = "Done!"
+  },function(data){
+      $scope.status = "Error!"
+  });
+}
   // TODO in Lab 5: you need to get the dish according to the routing parameter
   // $routingParams.paramName
   // Check the app.js to figure out what is the paramName in this case
-  
+  $scope.getInfo();
 });
--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -4,7 +4,7 @@ dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
     $scope.search = function(query,type) {
       $scope.status = "Searching...";
       Dinner.DishSearch.get({query:query,type:type},function(data){
-        $scope.dishes=data.results;
+        $scope.dishes = data.results;
         $scope.status = "Showing " + data.results.length + " results";
       },function(data){
         $scope.status = "There was an error";
